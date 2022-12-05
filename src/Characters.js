dd163aa47b4520d6e1c3f5819cad2a49d6922738
MandeepMann <mandeep_mann30@hotmail.com>
2022-12-04 21:38:08 -0500

    netlify error fix

--- src/Characters.js
+++ src/Characters.js
@@ -1,20 +1,5 @@
 import { Fragment } from "react";
-import warriors from "./warriors.js";
-
-// const showImage = (characterList) => {
-//     const showImage = (prevCharacter) => {
-//     const src = "./assets/warriorAssets/hero.png"
-//     if (prevCharacter.character.userSelection = "Hero") {
-//         src = './assets/warriorAssets/hero.png'
-//     } else if (prevCharacter.character.userSelection = "Paladin") {
-//         src = './assets/warriorAssets/paladin.png'
-//     } else if (prevCharacter.character.userSelection = "Dark Knight") {
-//         src = './assets/warriorAssets/darkKnight.png'
-//     }
-// }
-// }
-
-
+// import warriors from "./warriors.js";
 
 const Characters = ({characterList}) => {
     console.log(characterList)
@@ -25,7 +10,7 @@ const Characters = ({characterList}) => {
                 return (
                     <li key={prevCharacter.key}>
                         <p>{prevCharacter.character.userCharName}</p>
-                        <img src='' alt={`A picture of a ${prevCharacter.character.userSelection}`} />
+                        <img src='' alt={`A ${prevCharacter.character.userSelection}`} />
                         <ul>
                             <li>Class: {prevCharacter.character.userSelection}</li>
                         </ul>
