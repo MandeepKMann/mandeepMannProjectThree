7757775de38f3b1a17d2657fe9b69d53329a0529
MandeepMann <mandeep_mann30@hotmail.com>
2022-12-07 16:48:21 -0500

    fixed comments and class names for header

--- src/Header.js
+++ src/Header.js
@@ -9,27 +9,28 @@ const Header = () => {
 
     return (
         <header>
-                <div className="wrapper">
-                    <div className="headerFlex">
-                        <div className="headerContent">
-                            <div className="textContainer">
-                            <h1 className="title">Path of the Warrior</h1>
+            <div className="wrapper">
+                <div className="headerFlex">
+                    <div className="headerContent">
+                        <div className="headerTextContainer">
+                            <h1 className="headerTitle">Path of the Warrior</h1>
                             <p>Did you come here to become a Warrior?</p>
                             <p>Warriors have great strength and high HP, and face their enemies up-close with powerful attacks. Sounds exiting, no?</p>
                             <p>You look like you are more than qualified. If you wish to become a Warrior, I welcome you. Is that your wish?</p>
                             <p>Your heart is ready, yes? There is no room for hesitation in a Warrior's soul.</p>
-                            </div>
-                            <div className="backgroundImage">
+                        </div>{/* END .textContainer */}
+                            <div className="headerImage">
                                 <img src={explorers} alt="Five explorers with different classes (mage, thief, pirate, warrior, archer)" />
-                            </div>
-                        </div>
-
-                        <Link to="warriorsinfo">
-                            <button>I'm Ready</button>
-                        </Link>
-                    </div>
-                </div>
-            </header>
+                            </div>{/* END .headerImage */}
+                    </div>{/* END .headerContent */}
+                        
+                    {/* Link to WarrioInfo.js */}
+                    <Link to="warriorsinfo">
+                        <button>I'm Ready</button>
+                    </Link>
+                </div>{/* END .headerFlex */}
+            </div>
+        </header>
     )
 }
 
