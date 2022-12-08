6c27ccfb78e487d340b4d9ea20789b8ae46dbb19
MandeepMann <mandeep_mann30@hotmail.com>
2022-12-07 19:45:34 -0500

    fixed media queries and Link for Error.js

--- src/Error.js
+++ src/Error.js
@@ -7,12 +7,16 @@ import blackMage from "./assets/blackMage.png"
 
 const Error = () => {
     return (
-        <section className="Error">
-            <h1 className="errorMessage">...uh you're not supposed to be here yet</h1>
-            <img src={blackMage} alt="A dark figure in a black robe" />
-            <Link to="/">
-                <button>Escape!</button>
-            </Link>
+        <section className="error">
+            <div className="wrapper errorFlex">
+                <h1 className="errorMessage">...uh you're not supposed to be here yet</h1>
+                <div className="errorImage">
+                    <img src={blackMage} alt="A dark figure in a black robe" />
+                </div>
+                <Link to="/" className="routerLink" id="escape">
+                    Escape!
+                </Link>
+            </div>
         </section>
     )
 }
